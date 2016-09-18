0a08c5e8863f35b408fb0ed0236db25b70e3cb24
Masayuki Higashino <msyk@hgsn.info>
2016-09-18 19:15:04 +0900

    bundle gem thinreports-template-cli

---
+++ lib/thinreports/template/cli.rb
@@ -0,0 +1,9 @@
+require "thinreports/template/cli/version"
+
+module Thinreports
+  module Template
+    module Cli
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/thinreports/template/cli/version.rb
@@ -0,0 +1,7 @@
+module Thinreports
+  module Template
+    module Cli
+      VERSION = "0.1.0"
+    end
+  end
+end
