08f3ce5fe6f8b63ed80a230c18f0735f1ddbf941
Masayuki Higashino <msyk@hgsn.info>
2017-05-30 16:53:34 +0900

    improve displaying the table view

--- lib/thinreports/template/cli/executor.rb
+++ lib/thinreports/template/cli/executor.rb
@@ -27,18 +27,19 @@ module Thinreports; module Template; module CLI; class Executor
 
   def info
     return Terminal::Table.new(title:@report.default_layout.format.report_title) do |t|
-      t << %w(b:id b:type b:display? bf:value tb:has_format? tb:format_type)
-      t << :separator
-      @report.default_layout.format.shapes.values.map { |s|
-        record = [s.id, s.type, s.display?, s.value]
-        case s.type
-        when Thinreports::Core::Shape::TextBlock::TYPE_NAME
-          record += [s.has_format?, s.format_type]
-        else
-          record += [nil, nil]
+      textblocks = @report.default_layout.format.shapes.select{ |_, v|
+        v.type == Thinreports::Core::Shape::TextBlock::TYPE_NAME
+      }
+      if !textblocks.empty?
+        t << %w(id ref_id display? multiple? value format_base format_type format_value description)
+        t << :separator
+        textblocks.each_value do |s|
+          format_value = ''
+          format_value << s.format_datetime_format if s.format_datetime_format
+          format_value << "delimiter=[#{s.format_number_delimiter}]" if s.format_number_delimiter
+          format_value << "/precision=[#{s.format_number_precision}]" if s.format_number_precision
+          t << [s.id, s.ref_id, s.display?, s.multiple?, s.value, s.format_base, s.format_type, format_value, s.attributes['description']]
         end
-      }.each do |s|
-        t << s
       end
     end
   end
