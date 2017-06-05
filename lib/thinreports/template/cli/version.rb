03d4dc6529d19d2c13bd56e66247cc56099d7a81
Masayuki Higashino <msyk@hgsn.info>
2017-06-05 21:44:15 +0900

    cleanup

--- lib/thinreports/template/cli/version.rb
+++ lib/thinreports/template/cli/version.rb
@@ -1,3 +1,7 @@
-module Thinreports; module Template; module CLI
-  VERSION = '0.2.0'
-end; end; end
+module Thinreports
+  module Template
+    module CLI
+      VERSION = '0.2.0'.freeze
+    end
+  end
+end
