7a17a83551c906e2309a963ee6da669cf51acc82
Masayuki Higashino <msyk@hgsn.info>
2017-06-09 03:04:57 +0900

    add tests

--- test/thinreports-template-cli_test.rb
+++ test/thinreports-template-cli_test.rb
@@ -7,10 +7,10 @@ class Thinreports::TemplateCLI::Test < Minitest::Test
   end
 
   #  TODO
-  # def test_json
-  #   assert_output("std capture\n") {
-  #     Thinreports::TemplateCLI::CLI.start(['test/fixtures/sample.tlf', '--format=json'])
-  #   }
-  # end
+  def test_json
+    assert_output('[{"id":"date","ref_id":"","display":true,"multiple":false,"default_value":"","option_value":"2017-01-01T00:00:00+00:00","real_value":"2017/01/01 00:00:00","fmt_base":"","fmt_type":"datetime","fmt_value":"%Y/%m/%d %H:%M:%S","desc":"Date"},{"id":"subject","ref_id":"","display":true,"multiple":false,"default_value":"","option_value":"","real_value":null,"fmt_base":"","fmt_type":"","fmt_value":"","desc":"Subject"},{"id":"name","ref_id":"","display":true,"multiple":false,"default_value":"","option_value":"","real_value":null,"fmt_base":"","fmt_type":"","fmt_value":"","desc":"Name"},{"id":"number","ref_id":"","display":true,"multiple":false,"default_value":"","option_value":"","real_value":null,"fmt_base":"","fmt_type":"number","fmt_value":"delimiter=[,]/precision=[0]","desc":"Number"},{"id":"date_jp","ref_id":"","display":true,"multiple":false,"default_value":"","option_value":"2017-01-01T00:00:00+00:00","real_value":"平成29年01月01日 00時00分00秒","fmt_base":"","fmt_type":"datetime","fmt_value":"%O%E年%m月%d日 %H時%M分%S秒","desc":"Date"}]'+$/) {
+      Thinreports::TemplateCLI::CLI.start(['test/fixtures/sample.tlf', '--format=json', "--date='2017-01-01'", "--date_jp='2017-01-01'"])
+    }
+  end
 
 end
