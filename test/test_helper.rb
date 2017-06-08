cea4f44363e6e4fe517db7f35de21e79a2c2e019
Masayuki Higashino <msyk@hgsn.info>
2017-06-09 04:26:25 +0900

    update assets for testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,5 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'thinreports/template-cli'
 require 'minitest/reporters'
-Minitest::Reporters.use!
+Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(color:true)]
 require 'minitest/autorun'
